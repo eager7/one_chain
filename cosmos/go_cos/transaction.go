8d6eaa5dfcb82f40a44d4f690d00b8b4fd125212
eager7 <panchangtao@gmail.com>
2019-05-30 16:02:17 +0800

    将交易进行分组

--- cosmos/go_cos/transaction.go
+++ cosmos/go_cos/transaction.go
@@ -41,115 +41,150 @@ func (c *CosCli) ParseTransaction(block *core_types.ResultBlock) (txs []Transact
 func (c *CosCli) HandleTransactions(txs []Transaction) error {
 	for _, tx := range txs {
 		for _, m := range tx.Tx.Msgs {
-			switch m.Type() {
-			case "send":
-				msg, ok := m.(bank.MsgSend)
-				if !ok {
-					return errors.New("can't convert to send")
+			switch m.Route() {
+			case "bank":
+				switch m.Type() {
+				case "send":
+					msg, ok := m.(bank.MsgSend)
+					if !ok {
+						return errors.New("can't convert to send")
+					}
+					log.Debug(tx.Receipt.Height, "send-->from:", msg.FromAddress.String(), "to:", msg.ToAddress.String(), "amount:", msg.Amount)
+				case "multisend":
+					msg, ok := m.(bank.MsgMultiSend)
+					if !ok {
+						return errors.New("can't convert to multisend")
+					}
+					log.Debug(tx.Receipt.Height, "multisend-->inputs:", msg.Inputs, "outputs:", msg.Outputs)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "send-->from:", msg.FromAddress.String(), "to:", msg.ToAddress.String(), "amount:", msg.Amount)
-			case "begin_unbonding":
-				msg, ok := m.(types.MsgUndelegate)
-				if !ok {
-					return errors.New("can't convert to begin_unbonding")
+			case "staking":
+				switch m.Type() {
+				case "delegate":
+					msg, ok := m.(staking.MsgDelegate)
+					if !ok {
+						return errors.New("can't convert to delegate")
+					}
+					log.Debug(tx.Receipt.Height, "delegate-->delegator_address:", msg.DelegatorAddress, "validator_address:", msg.ValidatorAddress, "amount:", msg.Amount)
+				case "edit_validator":
+					msg, ok := m.(staking.MsgEditValidator)
+					if !ok {
+						return errors.New("can't convert to delegate")
+					}
+					log.Debug(tx.Receipt.Height, "edit_validator-->moniker:", msg.Moniker, "identity:", msg.Identity, "website:", msg.Website, "details:", msg.Details, "address:",
+						msg.ValidatorAddress, "commission_rate:", msg.CommissionRate, "min_self_delegation:", msg.MinSelfDelegation)
+				case "begin_unbonding":
+					msg, ok := m.(types.MsgUndelegate)
+					if !ok {
+						return errors.New("can't convert to begin_unbonding")
+					}
+					log.Debug(tx.Receipt.Height, "begin_unbonding-->delegate:", msg.DelegatorAddress.String(), "validator:", msg.ValidatorAddress.String(), "amount:", msg.Amount)
+				case "begin_redelegate":
+					msg, ok := m.(staking.MsgBeginRedelegate)
+					if !ok {
+						return errors.New("can't convert to begin_redelegate")
+					}
+					log.Debug(tx.Receipt.Height, "begin_redelegate-->delegator_address:", msg.DelegatorAddress, "validator_src_address:", msg.ValidatorSrcAddress, "validator_dst_address:", msg.ValidatorDstAddress, "amount:", msg.Amount)
+				case "create_validator":
+					msg, ok := m.(staking.MsgCreateValidator)
+					if !ok {
+						return errors.New("can't convert to create_validator")
+					}
+					log.Debug(tx.Receipt.Height, "create_validator-->description:", msg.Description, "commission:", msg.Commission, "min_self_delegation:", msg.MinSelfDelegation,
+						"delegator_address:", msg.DelegatorAddress, "validator_address:", msg.ValidatorAddress, "pubkey:", msg.PubKey, "value:", msg.Value)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "begin_unbonding-->delegate:", msg.DelegatorAddress.String(), "validator:", msg.ValidatorAddress.String(), "amount:", msg.Amount)
-			case "withdraw_delegator_reward":
-				msg, ok := m.(draw.MsgWithdrawDelegatorReward)
-				if !ok {
-					return errors.New("can't convert to withdraw_delegator_reward")
+			case "distr":
+				switch m.Type() {
+				case "set_withdraw_address":
+					msg, ok := m.(draw.MsgSetWithdrawAddress)
+					if !ok {
+						return errors.New("can't convert to set_withdraw_address")
+					}
+					log.Debug(tx.Receipt.Height, "set_withdraw_address-->delegator_address:", msg.DelegatorAddress, "withdraw_address:", msg.WithdrawAddress)
+				case "withdraw_delegator_reward":
+					msg, ok := m.(draw.MsgWithdrawDelegatorReward)
+					if !ok {
+						return errors.New("can't convert to withdraw_delegator_reward")
+					}
+					log.Debug(tx.Receipt.Height, "withdraw_delegator_reward-->delegator_address:", msg.DelegatorAddress.String(), "validator_address:", msg.ValidatorAddress.String())
+				case "withdraw_validator_rewards_all":
+					msg, ok := m.(draw.MsgWithdrawValidatorCommission)
+					if !ok {
+						return errors.New("can't convert to withdraw_validator_rewards_all")
+					}
+					log.Debug(tx.Receipt.Height, "withdraw_validator_rewards_all-->validator_address:", msg.ValidatorAddress)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "withdraw_delegator_reward-->delegator_address:", msg.DelegatorAddress.String(), "validator_address:", msg.ValidatorAddress.String())
-			case "withdraw_validator_rewards_all":
-				msg, ok := m.(draw.MsgWithdrawValidatorCommission)
-				if !ok {
-					return errors.New("can't convert to withdraw_validator_rewards_all")
+			case "gov":
+				switch m.Type() {
+				case "vote":
+					msg, ok := m.(gov.MsgVote)
+					if !ok {
+						return errors.New("can't convert to vote")
+					}
+					log.Debug(tx.Receipt.Height, "vote-->proposal_id:", msg.ProposalID, "voter:", msg.Voter, "option:", msg.Option.String())
+				case "deposit":
+					msg, ok := m.(gov.MsgDeposit)
+					if !ok {
+						return errors.New("can't convert to deposit")
+					}
+					log.Debug(tx.Receipt.Height, "deposit-->proposal_id:", msg.ProposalID, "depositor:", msg.Depositor, "amount:", msg.Amount)
+				case "submit_proposal":
+					msg, ok := m.(gov.MsgSubmitProposal)
+					if !ok {
+						return errors.New("can't convert to submit_proposal")
+					}
+					log.Debug(tx.Receipt.Height, "submit_proposal-->title:", msg.Title, "description:", msg.Description,
+						"proposal_type:", msg.ProposalType.String(), "proposer:", msg.Proposer.String(), "initial_deposit:", msg.InitialDeposit.String())
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "withdraw_validator_rewards_all-->validator_address:", msg.ValidatorAddress)
-			case "delegate":
-				msg, ok := m.(staking.MsgDelegate)
-				if !ok {
-					return errors.New("can't convert to delegate")
+			case "ibc":
+				switch m.Type() {
+				case "receive":
+					msg, ok := m.(ibc.MsgIBCReceive)
+					if !ok {
+						return errors.New("can't convert to receive")
+					}
+					log.Debug(tx.Receipt.Height, "receive-->src_addr:", msg.SrcAddr, "dest_addr:", msg.DestAddr, "coins:", msg.Coins, "src_chain:",
+						msg.SrcChain, "dest_chain:", msg.DestChain, "address:", msg.Relayer, "sequence:", msg.Sequence)
+				case "transfer":
+					msg, ok := m.(ibc.MsgIBCTransfer)
+					if !ok {
+						return errors.New("can't convert to receive")
+					}
+					log.Debug(tx.Receipt.Height, "receive-->src_addr:", msg.SrcAddr, "dest_addr:", msg.DestAddr, "coins:", msg.Coins, "src_chain:", msg.SrcChain, "dest_chain:", msg.DestChain)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "delegate-->delegator_address:", msg.DelegatorAddress, "validator_address:", msg.ValidatorAddress, "amount:", msg.Amount)
-			case "edit_validator":
-				msg, ok := m.(staking.MsgEditValidator)
-				if !ok {
-					return errors.New("can't convert to delegate")
+			case "slashing":
+				switch m.Type() {
+				case "unjail":
+					msg, ok := m.(slashing.MsgUnjail)
+					if !ok {
+						return errors.New("can't convert to unjail")
+					}
+					log.Debug(tx.Receipt.Height, "unjail-->address:", msg.ValidatorAddr)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "edit_validator-->moniker:", msg.Moniker, "identity:", msg.Identity, "website:", msg.Website, "details:", msg.Details, "address:",
-					msg.ValidatorAddress, "commission_rate:", msg.CommissionRate, "min_self_delegation:", msg.MinSelfDelegation)
-			case "begin_redelegate":
-				msg, ok := m.(staking.MsgBeginRedelegate)
-				if !ok {
-					return errors.New("can't convert to begin_redelegate")
+			case "crisis":
+				switch m.Type() {
+				case "verify_invariant":
+					msg, ok := m.(crisis.MsgVerifyInvariant)
+					if !ok {
+						return errors.New("can't convert to verify_invariant")
+					}
+					log.Debug(tx.Receipt.Height, "verify_invariant-->sender:", msg.Sender.String(), "invariant_module_name:", msg.InvariantModuleName, "invariant_route:", msg.InvariantRoute)
+				default:
+					return errors.New("unknown msg type:" + m.Type())
 				}
-				log.Debug(tx.Receipt.Height, "begin_redelegate-->delegator_address:", msg.DelegatorAddress, "validator_src_address:", msg.ValidatorSrcAddress, "validator_dst_address:", msg.ValidatorDstAddress, "amount:", msg.Amount)
-			case "create_validator":
-				msg, ok := m.(staking.MsgCreateValidator)
-				if !ok {
-					return errors.New("can't convert to create_validator")
-				}
-				log.Debug(tx.Receipt.Height, "create_validator-->description:", msg.Description, "commission:", msg.Commission, "min_self_delegation:", msg.MinSelfDelegation,
-					"delegator_address:", msg.DelegatorAddress, "validator_address:", msg.ValidatorAddress, "pubkey:", msg.PubKey, "value:", msg.Value)
-			case "vote":
-				msg, ok := m.(gov.MsgVote)
-				if !ok {
-					return errors.New("can't convert to vote")
-				}
-				log.Debug(tx.Receipt.Height, "vote-->proposal_id:", msg.ProposalID, "voter:", msg.Voter, "option:", msg.Option.String())
-			case "deposit":
-				msg, ok := m.(gov.MsgDeposit)
-				if !ok {
-					return errors.New("can't convert to deposit")
-				}
-				log.Debug(tx.Receipt.Height, "deposit-->proposal_id:", msg.ProposalID, "depositor:", msg.Depositor, "amount:", msg.Amount)
-			case "receive":
-				msg, ok := m.(ibc.MsgIBCReceive)
-				if !ok {
-					return errors.New("can't convert to receive")
-				}
-				log.Debug(tx.Receipt.Height, "receive-->src_addr:", msg.SrcAddr, "dest_addr:", msg.DestAddr, "coins:", msg.Coins, "src_chain:",
-					msg.SrcChain, "dest_chain:", msg.DestChain, "address:", msg.Relayer, "sequence:", msg.Sequence)
-			case "transfer":
-				msg, ok := m.(ibc.MsgIBCTransfer)
-				if !ok {
-					return errors.New("can't convert to receive")
-				}
-				log.Debug(tx.Receipt.Height, "receive-->src_addr:", msg.SrcAddr, "dest_addr:", msg.DestAddr, "coins:", msg.Coins, "src_chain:", msg.SrcChain, "dest_chain:", msg.DestChain)
-			case "multisend":
-				msg, ok := m.(bank.MsgMultiSend)
-				if !ok {
-					return errors.New("can't convert to multisend")
-				}
-				log.Debug(tx.Receipt.Height, "multisend-->inputs:", msg.Inputs, "outputs:", msg.Outputs)
-			case "set_withdraw_address":
-				msg, ok := m.(draw.MsgSetWithdrawAddress)
-				if !ok {
-					return errors.New("can't convert to set_withdraw_address")
-				}
-				log.Debug(tx.Receipt.Height, "set_withdraw_address-->delegator_address:", msg.DelegatorAddress, "withdraw_address:", msg.WithdrawAddress)
-			case "submit_proposal":
-				msg, ok := m.(gov.MsgSubmitProposal)
-				if !ok {
-					return errors.New("can't convert to submit_proposal")
-				}
-				log.Debug(tx.Receipt.Height, "submit_proposal-->title:", msg.Title, "description:", msg.Description,
-					"proposal_type:", msg.ProposalType.String(), "proposer:", msg.Proposer.String(), "initial_deposit:", msg.InitialDeposit.String())
-			case "unjail":
-				msg, ok := m.(slashing.MsgUnjail)
-				if !ok {
-					return errors.New("can't convert to unjail")
-				}
-				log.Debug(tx.Receipt.Height, "unjail-->address:", msg.ValidatorAddr)
-			case "verify_invariant":
-				msg, ok := m.(crisis.MsgVerifyInvariant)
-				if !ok {
-					return errors.New("can't convert to verify_invariant")
-				}
-				log.Debug(tx.Receipt.Height, "verify_invariant-->sender:", msg.Sender.String(), "invariant_module_name:", msg.InvariantModuleName, "invariant_route:", msg.InvariantRoute)
 			default:
-				return errors.New("unknown msg type:" + m.Type())
+				return errors.New("unknown msg route:" + m.Route())
 			}
 		}
 	}
--- cosmos/sql/cosmos_database.sql
+++ cosmos/sql/cosmos_database.sql
@@ -58,34 +58,6 @@ CREATE TABLE IF NOT EXISTS cosmos_db.t_transaction_info (
     KEY `value` (`value`)           USING BTREE
 )ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
 
-
-# alter table eth_database.t_transfer_info add `tx_id` DOUBLE(12,4) NOT NULL DEFAULT 0 COMMENT '内联交易的全局ID，用区块高度和log偏移量组成的浮点数';
-# alter table eth_database.t_transfer_info add index `tx_id`(`tx_id`) USING BTREE;
-CREATE TABLE IF NOT EXISTS cosmos_db.t_transfer_info (
-`id`                BIGINT UNSIGNED     NOT NULL AUTO_INCREMENT COMMENT '自增主键',
-`address`           CHAR(40)            NOT NULL DEFAULT ''     COMMENT '合约地址',
-`block_hash`        CHAR(64)            NOT NULL DEFAULT ''     COMMENT '当前交易所在区块的哈希',
-`block_number`      BIGINT UNSIGNED     NOT NULL DEFAULT 0      COMMENT '当前交易所在区块的高度',
-`timestamp`         INT UNSIGNED        NOT NULL DEFAULT 0      COMMENT '交易发生的时间点',
-`log_index`         INT UNSIGNED        NOT NULL DEFAULT 0      COMMENT 'log位置',
-`from`              CHAR(40)            NOT NULL DEFAULT ''     COMMENT '当前交易发起者',
-`to`                CHAR(40)            NOT NULL DEFAULT ''     COMMENT '当前交易接收者',
-`hash`              CHAR(64)            NOT NULL DEFAULT ''     COMMENT '当前交易哈希值',
-`tx_id`             DOUBLE(12,4)        NOT NULL DEFAULT 0      COMMENT '内联交易的全局ID，用区块高度和log偏移量组成的浮点数',
-`input_flag`        TINYINT UNSIGNED    NOT NULL DEFAULT 1      COMMENT '标识input数据位置，1表示从数据库取，2表示从链上取',
-`input`             VARCHAR(15360)      NOT NULL DEFAULT ''     COMMENT '交易输入数据，如果to是合约，这里存放合约调用方法和参数，最大15K',
-`transaction_index` SMALLINT UNSIGNED   NOT NULL DEFAULT 0      COMMENT '交易在区块中的偏移量',
-`value`             VARCHAR(64)         NOT NULL DEFAULT ''     COMMENT '交易金额',
-PRIMARY KEY (`id`),
-UNIQUE KEY `hash`(`hash`,log_index),
-KEY `tx_id`(`tx_id`) USING BTREE ,
-KEY `number`(`block_number`) USING BTREE ,
-KEY `from_to`(`from`(10)) USING BTREE ,
-KEY `to_from`(`to`(10)) USING BTREE ,
-KEY `block_hash`(`block_hash`(10)) USING BTREE ,
-KEY `value`(`value`) USING BTREE
-)ENGINE=INNODB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS cosmos_db.t_asserts_info (
 `id`                BIGINT UNSIGNED             NOT NULL AUTO_INCREMENT     COMMENT '自增主键',
 `address`           CHAR(40)                    NOT NULL DEFAULT ''         COMMENT '账户地址',
